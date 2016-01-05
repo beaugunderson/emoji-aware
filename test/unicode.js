464b8e3fa9e52f69294afe0fd35013d0497d29c9
Beau Gunderson <beau@beaugunderson.com>
2016-01-04 23:14:56 -0800

    add tests

--- test/unicode.js
+++ test/unicode.js
@@ -5,6 +5,16 @@ import test from 'ava';
 var characters = require('./fixtures/emoji-characters.js');
 var unicode = require('../parsers/unicode-and-emoji.js');
 
+test('parseOne emoji', t => {
+  t.plan(characters.length);
+
+  characters.forEach(function (e) {
+    var result = unicode.parseOne(e);
+
+    t.is(result, e);
+  });
+});
+
 test('parse string that includes emoji', t => {
   t.plan(characters.length * 7);
 
@@ -19,4 +29,12 @@ test('parse string that includes emoji', t => {
     t.is(result[5], 'f');
     t.is(result[6], 'g');
   });
+});
+
+test('parse unicode string', t => {
+  t.plan(1);
+
+  var result = unicode.parse('Testing «ταБЬℓσ»: 1<2 & 4+1>3, now 20% off!');
+
+  t.is(result[0], 'T');
 });
