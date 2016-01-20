e2c3a824ec6e28c2e86e9191fd7123cea8ac1e00
Beau Gunderson <beau@beaugunderson.com>
2016-01-20 11:38:32 -0800

    create tests per character (much slower)

--- test/emoji.js
+++ test/emoji.js
@@ -6,30 +6,38 @@ var characters = require('./fixtures/emoji-characters.js');
 var emoji = require('../parsers/emoji.js');
 var isEmoji = require('../lib/is-emoji.js');
 
-test('parse one emoji', function (t) {
-  t.plan(characters.length);
-
-  characters.forEach(function (e) {
+characters.forEach(function (e) {
+  test('parse one emoji ' + e, function (t) {
+    t.plan(1);
     t.is(emoji.parse(e)[0], e);
   });
-});
 
-test('parse two emoji', function (t) {
-  t.plan(characters.length * 2);
+  test('parse two emoji ' + e + e, function (t) {
+    t.plan(2);
 
-  characters.forEach(function (e) {
     var result = emoji.parse(e + e);
 
     t.is(result[0], e);
     t.is(result[1], e);
   });
-});
 
-test('parseOne emoji', function (t) {
-  t.plan(characters.length);
-
-  characters.forEach(function (e) {
+  test('parseOne emoji ' + e, function (t) {
+    t.plan(1);
     t.is(emoji.parseOne(e), e);
+  });
+
+  test('parse fail on emoji with non-emoji a' + e + 'a', function (t) {
+    t.plan(1);
+
+    var result = emoji.parse(`a${e}a`);
+
+    t.false(result);
+  });
+
+  test('isEmoji emoji', function (t) {
+    t.plan(1);
+
+    t.true(isEmoji(e));
   });
 });
 
@@ -37,24 +45,6 @@ test('parseOne fail non-emoji', function (t) {
   t.plan(1);
 
   t.false(emoji.parseOne('a'));
-});
-
-test('parse fail on emoji with non-emoji', function (t) {
-  t.plan(characters.length);
-
-  characters.forEach(function (e) {
-    var result = emoji.parse(`a${e}a`);
-
-    t.false(result);
-  });
-});
-
-test('isEmoji emoji', function (t) {
-  t.plan(characters.length);
-
-  characters.forEach(function (e) {
-    t.true(isEmoji(e));
-  });
 });
 
 test('isEmoji fail on non-emoji', function (t) {
